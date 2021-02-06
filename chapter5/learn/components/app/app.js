92e0a4f6393022225bd3c2b97d03a42b469f84f9
Dominic Chin <dominicchin@yahoo.com>
2021-02-06 20:27:15 +0800

    add mouse move

--- chapter5/learn/components/app/app.js
+++ chapter5/learn/components/app/app.js
@@ -4,6 +4,18 @@ class WebHarpApp extends HTMLElement {
 	connectedCallback() {
 		this.innerHTML = '<webharp-strings strings="' + 
 			this.getAttribute('strings') + '"></webharp-strings>';
+		this.stringElements = this.querySelector('webharp-strings');
+		this.addEventListener('mousemove', e => this.onMouseMove(e));
+	}
+	onMouseMove (event) {
+		this.stringElements.points = {
+			last: this.lastPoint,
+			current: { x: event.pageX, y: event.pageY }
+		};
+		this.lastPoint = {
+			x: event.pageX,
+			y: event.pageY
+		};
 	}
 }
 if (!customElements.get('webharp-app')){
